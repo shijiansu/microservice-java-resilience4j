62b6998c99db704db40038adf9c16d6734b29219
Shijian Su <sushijian.iss.nus@gmail.com>
2020-06-18 00:37:49 +0800

    Update multi threads pool control in test

--- resilience4j-first-try/ratelimiter/src/test/java/microservice/java/resilience4j/MultithreadingTestTools.java
+++ resilience4j-first-try/ratelimiter/src/test/java/microservice/java/resilience4j/MultithreadingTestTools.java
@@ -8,10 +8,10 @@ import java.util.concurrent.ForkJoinPool;
 public abstract class MultithreadingTestTools {
   // https://blog.csdn.net/j16421881/article/details/85471678
   public void submitToForkJoinPool(int concurrency, Runnable task) {
-    // 例如, 默认为4
+    // 默认会使用CPU数量减去1
     int defaultConcurrency = ForkJoinPool.commonPool().getParallelism();
-    // 创建一个线程数为不大于默认线程数的线程池
-    ForkJoinPool forkJoinPool = new ForkJoinPool(min(concurrency, defaultConcurrency));
+    // 创建一个线程数为不大于(默认线程数的线程池+1)*2, 即2倍于CPU的线程
+    ForkJoinPool forkJoinPool = new ForkJoinPool(min(concurrency, (defaultConcurrency + 1) * 2));
     try {
       forkJoinPool.submit(task).get();
     } catch (InterruptedException | ExecutionException e) {
